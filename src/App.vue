542dde7c308db144b44073598c4f948f22685324
460 <460797765@qq.com>
2020-10-22 23:51:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+ <div id='app'>
+   <router-view/>
+   <FooterGuide v-show="$route.meta.showFooter"/>
+ </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  components:{
+    FooterGuide
+  }  
+}
+</script>
+
+<style lang="stylus">
+  #app{
+      width 100%
+      height 100%
+      background-color #f5f5f5
+    }
+</style>
